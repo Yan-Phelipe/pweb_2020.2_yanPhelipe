e15f09399cc9247557b57c13f938f0726faac678
Yan-Phelipe <yanphelipe_3@hotmail.com>
2021-04-12 23:04:19 -0300

    programando listar pessoas

--- cadpessoas_yanphelipe/src/main/java/br/com/yanphelipe/cadpessoas/controller/PessoaController.java
+++ cadpessoas_yanphelipe/src/main/java/br/com/yanphelipe/cadpessoas/controller/PessoaController.java
@@ -1,11 +1,14 @@
 package br.com.yanphelipe.cadpessoas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.yanphelipe.cadpessoas.model.Pessoa;
 import br.com.yanphelipe.cadpessoas.repositories.PessoaRepository;
 
 @Controller
@@ -18,5 +21,13 @@ public class PessoaController {
 	@GetMapping
 	public String index() {
 		return "index.html";
+	}
+	
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas() {
+		List<Pessoa> lista = pessoaRepo.findAll();
+		ModelAndView mav = new ModelAndView("listarPessoas");
+		mav.addObject("pessoas", lista);
+		return mav;
 	}
 }
