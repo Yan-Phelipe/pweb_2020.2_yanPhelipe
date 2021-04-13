4893050e341861c7efd73a7ae41e502d288ab4eb
Yan-Phelipe <yanphelipe_3@hotmail.com>
2021-04-12 22:10:32 -0300

    modelando pastas do projeto

--- cadpessoas_yanphelipe/src/main/java/br/com/yanphelipe/cadpessoas/controller/PessoaController.java
+++ cadpessoas_yanphelipe/src/main/java/br/com/yanphelipe/cadpessoas/controller/PessoaController.java
@@ -1,10 +1,22 @@
 package br.com.yanphelipe.cadpessoas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.yanphelipe.cadpessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
-
+	
+	@Autowired
+	PessoaRepository pessoaRepo;
+	
+	@GetMapping
+	public String index() {
+		return "index.html";
+	}
 }
