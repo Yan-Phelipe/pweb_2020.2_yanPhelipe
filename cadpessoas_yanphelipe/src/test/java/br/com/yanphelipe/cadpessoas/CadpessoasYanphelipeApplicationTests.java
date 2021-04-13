413f41803503e54ad0b58d03ec0c88e0da66c8ed
Yan-Phelipe <yanphelipe_3@hotmail.com>
2021-04-12 21:11:34 -0300

    Iniciando CRUD

---
+++ cadpessoas_yanphelipe/src/test/java/br/com/yanphelipe/cadpessoas/CadpessoasYanphelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yanphelipe.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasYanphelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
